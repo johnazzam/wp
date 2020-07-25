1cff5f355a1b0b7b42f259079718a057f07efb0f
s3767649 <48371525+johnazzam@users.noreply.github.com>
2020-07-25 11:34:10 +1000

    Upadte A2

--- a2/index.php
+++ a2/index.php
@@ -1,40 +1,64 @@
 <!DOCTYPE html>
 <html lang='en'>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Assignment 2</title>
-    
-    <!-- Keep wireframe.css for debugging, add your css to style.css -->
-    <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
-    <link id='stylecss' type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>">
-    <script src='../wireframe.js'></script>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Assignment 2</title>
+
+  <style>
+  <!-- Keep wireframe.css for debugging, add your css to style.css -->
+  <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
+  <link id='stylecss' type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>">
+  </style>
+  <script src='../wireframe.js'></script>
   </head>
+
+
 
   <body>
 
-    <header>
-      <div>Put company logo and name here</div>
-    </header>
 
-    <nav>
-      <div>Put website navigation links here</div>
-    </nav>
 
-    <main>
-      <article id='Website Under Construction'>
-    <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
-        <img src='../../media/website-under-construction.png' alt='Website Under Construction' />
-      </article>
-    </main>
+  <header>
+  <div>ANZAC Douglas Raymond Baker - Letters Home</div>
+  <img src="../image1.jpg" alt="D . R. Baker F . Co one of the soldiers photographed in The Queenslander Pictorial supplement to The Queenslander 1914.jpg">
+  </header>
 
-    <footer>
-      <div>&copy;<script>
-        document.write(new Date().getFullYear());
-      </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+
+
+
+  <nav>
+    <div id="navbar">
+    <a href="#home">Home</a>
+    <a href="#Introduction">Introduction</a>
+    <a href="#Letters and Post Cards">Letters and Post Cards</a>
+    <a href="#Action Places">Action Places</a>
+    <a href="#Links to related materials">Links to related materials</a>
+    <a href="#Comments">Comments</a>
+    </div>
+  </nav>
+
+
+
+
+  <main>
+    <article id='Website Under Construction'>
+    </article>
+
+  </main>
+
+
+
+
+
+  <footer>
+    <div>&copy;<script>
+      document.write(new Date().getFullYear());
+      </script> John Azzam, s3767649 and Individual created. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
       <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
       <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
-    </footer>
+  </footer>
 
   </body>
-</html>
+  </html>
